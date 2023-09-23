34aedf610fa2850d99f7eb14333df1ea26ec84b3
artless <jayanlas00@gmail.com>
2023-09-23 18:57:37 -0400

    resolving issues

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,42 @@ import './App.css'
 import AppHeader from './components/AppHeader'
 import PostCard from './components/PostCard'
 import './index.css'
+import CreatePostModal from './components/CreatePostModal'
+import AppHeader from './components/AppHeader'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>     
-      <AppHeader /> 
-      <PostCard />      
-      <PostCard />
-    </>
-  )
+    
+    <div className = "accountContainer">
+      <AppHeader/>
+        <AccountHeader/>
+
+      <div className='account-info'>
+          <div className = "username">
+            <AccountUsername user = {"Name"}/>
+          </div>
+          <div className='email'>
+            <AccountEmail email = {"Ladiesman217@prime.coom"}/>
+          </div>
+          <div className = "img">
+            <AccountPicture src ="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"/>
+          </div>
+          
+          <div className='counter'>
+            <AccountStats count = {23} />
+          </div>
+
+
+          
+        </div>
+
+    </div>
+
+
+  );
 }
 
 export default App
