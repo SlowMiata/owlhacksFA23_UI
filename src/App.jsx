d52c1241e2509e9de23121b4b79fe2b4ef71c1b6
artless <jayanlas00@gmail.com>
2023-09-23 14:04:28 -0400

    AppHeader, PostCard  added

--- src/App.jsx
+++ src/App.jsx
@@ -3,26 +3,17 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import AppHeader from './components/AppHeader'
+import PostCard from './components/PostCard'
+import './index.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>      
-      <AppHeader />
-      
-      <h2>Vite + React</h2>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <>     
+      <AppHeader /> 
+      <PostCard />      
+      <PostCard />
     </>
   )
 }
--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,42 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import '../App.css'
-
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
-}
-
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}
+import userimg from '../assets/user.png'
 
 function AppHeader() {
     return(
       <>
-        <div className='app-header'>
+        <div className='navbar'>
           <h1 className='line-up'>PotHole Findr</h1>
-          <div className="dropdown">
-            <button type='button' onClick={() => {
-              document.getElementById("myDropdown").classList.toggle("show");
-            }} className="dropbtn">Dropdown</button>
-            <div id="myDropdown" className="dropdown dropdown-content">
-              <a href="#">Link 1</a>
-              <a href="#">Link 2</a>
-              <a href="#">Link 3</a>
-            </div>
-          </div>
+          <button type='button' className='nav-button' onClick={() => {
+            document.getElementById("myDropdown").classList.toggle("show");
+          }}>
+            <img src={userimg} className='like-button'></img>
+          </button>
         </div>
       </>
     )
